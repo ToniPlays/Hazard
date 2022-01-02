3186ac6ac45142fe8b32178b429fc750401508c5
ToniPlays <tok.simoska@gmail.com>
2022-01-02 18:15:52 +0200

    Fixed Hazard premake

--- Hazard/premake5.lua
+++ Hazard/premake5.lua
@@ -1,6 +1,5 @@
-project "HazardEditor"
-
-	kind "WindowedApp"
+project "Hazard"
+	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
 	staticruntime "off"
@@ -8,52 +7,64 @@ project "HazardEditor"
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
+	pchsource "src/hzrpch.cpp"
+
 	files
 	{
 		"src/**.h",
-		"src/**.cpp"
+		"src/**.cpp",
+		"vendor/stb_image/**.h",
+		"vendor/stb_image/**.cpp",
+		"vendor/glm/glm/**.hpp",
+		"vendor/glm/glm/**.inl",
+		"vendor/VulkanMemoryAllocator/**.h",
+		"vendor/VulkanMemoryAllocator/**.cpp"
 	}
 
-	defines {
+	defines
+	{
+		"GLFW_INCLUDE_NONE",
 		"IMGUI_IMPL_OPENGL_LOADER_GLAD"
 	}
 
 	includedirs
 	{
-		"%{wks.location}/Hazard/vendor/spdlog/include",
-		"%{wks.location}/Hazard/src",
-		"%{wks.location}/Hazard/vendor",
-		"%{IncludeDir.glm}",
+		"src",
+		"vendor/spdlog/include",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.Entt}",
-		"%{IncludeDir.ImGuizmo}",
 		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.yaml_cpp}",
+		"%{IncludeDir.Mono}",
+		"%{IncludeDir.Vorbis}",
+		"%{IncludeDir.Minimp3}",
+		"%{IncludeDir.Libogg}",
+		"%{IncludeDir.OpenAL}",
+		"%{IncludeDir.glm}",
 		"%{IncludeDir.Box2D}",
-		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.stb_image}",
+		"%{IncludeDir.Entt}",
+		"%{IncludeDir.Assimp}",
+		"%{IncludeDir.yaml_cpp}",
 		"%{IncludeDir.VulkanSDK}",
-		"%{IncludeDir.VMA}",
-		"src"
+		"%{IncludeDir.VMA}"
 	}
 
-	links
-	{
-		"ImGui",
-		"Hazard",
-		"%{Library.Mono_Debug_Lib}",
-		"%{Library.Assimp_Lib}",
+	links {
 		"GLFW",
-		"Glad"
+		"Glad",
+		"yaml-cpp",
+		"Box2D"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
+		pchheader "hzrpch.h"
 		defines {
 			"HZR_PLATFORM_WINDOWS",
 			"HZR_INCLUDE_OPENGL",
 			"HZR_INCLUDE_VULKAN",
 			"HZR_INCLUDE_OPENAL"
 		}
+
 		links {
 			"%{Library.Vulkan}",
 			"%{Library.VulkanUtils}",
@@ -61,17 +72,14 @@ project "HazardEditor"
 		}
 
 	filter "system:macosx"
+		pchheader "src/hzrpch.h"
 		defines {
 			"HZR_PLATFORM_MACOS",
 			"HZR_INCLUDE_METAL"
 		}
-		links {
-			"IOKit.framework",
-			"CoreFoundation.framework",
-			"Cocoa.framework",
-			"OpenGL.framework",
-			"Metal.frameWork",
-			"MetalKit.framework"
+		removefiles {
+			"src/Platform/Rendering/Vulkan/**",
+			"src/Platform/Rendering/OpenGL/**"
 		}
 
 	filter "configurations:Debug"
@@ -79,17 +87,40 @@ project "HazardEditor"
 		runtime "Debug"
 		symbols "on"
 
-		postbuildcommands
-		{
-			"{COPYDIR} \"%{LibraryDir.VulkanSDK_DebugDLL}\" \"%{cfg.targetdir}\""
-		}
+		if os.host() == "windows" then
+			links {
+				"%{Library.ShaderC_Debug}",
+				"%{Library.SPIRV_Cross_Debug}",
+				"%{Library.SPIRV_Tools_Debug}",
+				"%{Library.SPIRV_Cross_GLSL_Debug}",
+			}
+		end
 
 	filter "configurations:Release"
 		defines "HZR_RELEASE"
 		runtime "Release"
 		optimize "on"
 
+
+		if os.host() == "windows" then
+			links {
+				"%{Library.ShaderC_Release}",
+				"%{Library.SPIRV_Cross_Release}",
+				"%{Library.SPIRV_Tools_Release}",
+				"%{Library.SPIRV_Cross_GLSL_Release}",
+			}
+		end
+
 	filter "configurations:Dist"
 		defines "HZR_DIST"
 		runtime "Release"
 		optimize "on"
+
+		if os.host() == "windows" then
+			links {
+				"%{Library.ShaderC_Release}",
+				"%{Library.SPIRV_Cross_Release}",
+				"%{Library.SPIRV_Tools_Release}",
+				"%{Library.SPIRV_Cross_GLSL_Release}",
+			}
+		end
